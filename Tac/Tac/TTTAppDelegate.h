037c6048ee983655955bdffed1b2266280b52089
KaL <KaL@Kalsons-MacBook-Air.local>
2014-07-29 09:35:49 -0400

    create app

---
+++ Tac/Tac/TTTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTTAppDelegate.h
+//  Tac
+//
+//  Created by KaL on 7/29/14.
+//  Copyright (c) 2014 Kalson Kalu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
